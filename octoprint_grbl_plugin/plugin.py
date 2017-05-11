1751774454ac89eb5cfda0d9770e5c5da775c14e
Michael Cooper <mic159@gmail.com>
2017-05-11 23:25:10 +1000

    Fix some more commands

--- octoprint_grbl_plugin/plugin.py
+++ octoprint_grbl_plugin/plugin.py
@@ -1,3 +1,4 @@
+import re
 import logging
 
 log = logging.getLogger(__name__)
@@ -5,15 +6,20 @@ log = logging.getLogger(__name__)
 def unsupported_commands(comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
     """
     Suppress certain commands, because Grbl cant handle them.
-    
-    M110 (reset line number)
-    M105 (get temperature)
     """
     if cmd.startswith('M110 '):
+        # Reset line numbers not supported in Grbl
         return None,
-    if cmd.startswith('M105'):
+    if cmd == 'M105':
         # Translate temperature for "gcode parameters" and "Realtime data"
         return '?$G'
+    if cmd == 'M400':
+        # Finishes all current moves and and thus clears the buffer.
+        # That's identical to G4 P0.
+        return 'G4 P0'
+    if cmd == 'M114':
+        # Get current position
+        return '?'
 
 
 def translate_ok(comm_instance, line, *args, **kwargs):
@@ -21,6 +27,17 @@ def translate_ok(comm_instance, line, *args, **kwargs):
     This plugin moves Grbl's ok from the end to the start.
     OctoPrint needs the 'ok' to be at the start of the line.
     """
+    if 'MPos' in line:
+        # <Idle,MPos:0.000,0.000,0.000,WPos:0.000,0.000,0.000,RX:3,0/0>
+        match = re.search(r'MPos:([\d\.]+),([\d\.]+),([\d\.]+)', line)
+        # OctoPrint records positions in some instances.
+        # It needs a different format. Put both on the same line so the GRBL info is not lost
+        # and is accessible for "controls" to read.
+        return 'ok X:{0} Y:{1} Z:{2} {original}'.format(
+            *match.groups(),
+            original=line
+        )
+
     if not line.rstrip().endswith('ok'):
         return line
 
