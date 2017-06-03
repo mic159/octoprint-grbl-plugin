af8f673d6997142d614d3615e59b9b9522b698a7
Michael Cooper <mic159@gmail.com>
2017-06-03 21:00:25 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='octoprint-grbl-plugin',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     license='MIT',
     author='mic159',
